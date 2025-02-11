86974d7243d90f45f51666be5faf8b371c68182e
pro-grammer-SD <geniussantu1983@gmail.com>
2025-02-11 19:46:30 +0530

    update a bit more

--- example.py
+++ example.py
@@ -3,3 +3,11 @@ import mcsl
 server = mcsl.ServerEntity()
 # suggest papermc
 server.launch(loader_type="papermc", gui=True, min_mem=512, max_mem=2048)
+
+class ClassName(object):
+    """docstring for ClassName."""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+    arg
+
+    
